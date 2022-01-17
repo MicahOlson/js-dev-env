549e060e6779b4f0ce04cd225ab630e80bc3f3fa
Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
2022-01-17 12:36:59 -0800

    Resolve security vulnerabilities in dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,59 +1,47 @@
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
+const ESLintPlugin = require('eslint-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
-  const HtmlWebpackPlugin = require('html-webpack-plugin');
-  const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-  const Dotenv = require('dotenv-webpack');
 
-  module.exports = {
-    entry: './src/main.js',
-    output: {
-      filename: 'bundle.js',
-      path: path.resolve(__dirname, 'dist')
-    },
-    devtool: 'eval-source-map',
-    devServer: {               
-      contentBase: './dist'    
-    },
-    plugins: [
-      new CleanWebpackPlugin(),
-      new HtmlWebpackPlugin({
-        title: 'Change Me',
-        template: './src/index.html',
-        inject: 'body'
-      }),
-      new Dotenv()
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devtool: 'eval-source-map',
+  devServer: {
+    static: './dist',
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new Dotenv(),
+    new ESLintPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Webpack Dev Environment',
+      template: './src/index.html',
+      inject: 'body',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(png|svg|jpe?g|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.html$/,
+        use: ['html-loader'],
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
     ],
-    module: {
-      rules: [
-        {
-          test: /\.(gif|png|jpe?g)$/,
-          use: [
-            {
-              loader: 'file-loader',
-              options: {
-                name: '[name].[ext]',
-                outputPath: 'assets/images/'
-              }
-            }
-          ]
-        },
-        {
-          test:/\.html$/,
-          use: [
-            'html-loader'
-          ]
-        },
-        {
-          test: /\.css$/,
-          use: [
-            'style-loader',
-            'css-loader'
-          ]
-        },
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          loader: "eslint-loader"
-        }
-      ]
-    }
-  };
+  },
+};
